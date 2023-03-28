ffd21356db10ba611ad44235a2bd534c7b4d0c31
Isaac <isaacfarias006@gmail.com>
2023-03-27 21:47:03 -0300

    Adiciona classe campeonato

---
+++ src/models/Campeonato.java
@@ -0,0 +1,83 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Campeonato {
+    ArrayList<Clube> clubes;
+
+    public Campeonato(Clube... clubes) {
+        this.clubes = new ArrayList<Clube>();
+        for (Clube clube : clubes) {
+            this.clubes.add(clube);
+        }
+    }
+
+    public void jogarCampeonato() {
+        for (Clube clubeDaCasa : clubes) {
+            for (Clube clubeVisitante : clubes) {
+                if (clubeDaCasa.equals(clubeVisitante)) {
+                    continue;
+                }
+
+                jogarPartida(clubeDaCasa, clubeVisitante);
+            }
+        }
+    }
+
+    private void jogarPartida(Clube m, Clube v) {
+        int golsM = (int) Math.round((Math.random() * 10)) % 6;
+        int golsV = (int) Math.round((Math.random() * 10)) % 6;
+
+        if (golsM == golsV) {
+            m.empatar();
+            v.empatar();
+        }
+
+        if (golsM > golsV) {
+            int saldoGols = golsM - golsV;
+            m.ganhar(saldoGols);
+            v.perder(saldoGols);
+        }
+
+        if (golsV > golsM) {
+            int saldoGols = golsV - golsM;
+            v.ganhar(saldoGols);
+            m.perder(saldoGols);
+        }
+
+        // Para ficar fácil de entender o resultado final
+        System.out.println(m + " " + golsM + " x " + v + " " + golsV);
+    }
+
+    public String getClassificacao() {
+        Comparator<Clube> comp = new Comparator<Clube>() {
+            @Override
+            public int compare(Clube a, Clube b) {
+                // Compara 'b' com 'a' nessa ordem porque assim fica em ordem decrescente
+                int pontosComp = Integer.compare(b.pontos, a.pontos);
+
+                if (pontosComp != 0) {
+                    return pontosComp;
+                }
+
+                return Integer.compare(b.saldoGols, a.saldoGols);
+            }
+        };
+
+        Collections.sort(clubes, comp);
+
+        String classificacao = "";
+
+        for (Clube clube : clubes) {
+            classificacao += clube.toString() + " " + clube.pontos + " " + clube.saldoGols + "\n";
+        }
+
+        return classificacao;
+    }
+
+    public String getCampeao() {
+        return clubes.get(0).toString();
+    }
+}
